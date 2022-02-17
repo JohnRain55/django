407e988efb937f992a03e05646c204074835f505
John Rain55 <pasternak146@gmail.com>
2022-02-17 21:18:26 +0200

    Redirect Url HomeWork

--- my_page/geometry/views.py
+++ my_page/geometry/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from math import pi
+
 
 # Create your views here.
 
@@ -15,3 +16,9 @@ def get_square_area(request, width: int):
 
 def get_circle_area(request, radius: int):
     return HttpResponse(f'Square circle radius {radius} is {pi * radius * radius} ')
+
+
+def get_figure_area(request, figure, width, height=None):
+    if height:
+        return HttpResponseRedirect(f'/calculate_geometry/{figure}/{width}/{height}/')
+    return HttpResponseRedirect(f'/calculate_geometry/{figure}/{width}/')
