7eb96b8150e4282c8aad351b5d178b22d80aa308
John Rain55 <pasternak146@gmail.com>
2022-02-23 20:34:42 +0200

    Create main menu for horoscope

--- my_page/horoscope/views.py
+++ my_page/horoscope/views.py
@@ -20,17 +20,31 @@ zodiac_dict = {
 }
 
 
+def index(request):
+    zodiacs = list(zodiac_dict)
+    li_elements = ''
+    for sing in zodiacs:
+        redirect_path = reverse('horoscope-name', args=[sing])
+        li_elements += f"<li> <a href='{redirect_path}'>{sing.title()} </a> </li>"
+    response = f"""
+    <ul>
+        {li_elements}
+    </ul>
+    """
+    return HttpResponse(response)
+
+
 def get_info_about_sign_zodiac_by_number(request, sign_zodiac: int):
     zodiacs = list(zodiac_dict)
     if sign_zodiac > len(zodiac_dict):
         return HttpResponseNotFound(f"This is no {sign_zodiac} in zodiac")
     name_zodiac = zodiacs[sign_zodiac - 1]
-    redirect_url = reverse('horoscope-name', args=(name_zodiac, ))
+    redirect_url = reverse('horoscope-name', args=(name_zodiac,))
     return HttpResponseRedirect(redirect_url)
 
 
 def get_info_about_sign_zodiac(request, sign_zodiac: str):
     description = zodiac_dict.get(sign_zodiac, None)
     if description:
-        return HttpResponse(description)
+        return HttpResponse(f'<h2>{description}<h2/>')
     return HttpResponseNotFound(f"Брат, я не ебу шо такое {sign_zodiac}")
